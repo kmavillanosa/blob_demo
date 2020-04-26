1fc240f2d5ff339f64006ecd829b6f82add73196
Kim Avillanosa <avillanosakzxc@gmail.com>
2020-04-26 20:13:53 +0800

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blob_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        
+        [HttpGet("createFile")]
+        public IActionResult CreateFile([FromQuery]string[] logs)
+        {
+            try
+            {
+                var sb = new StringBuilder();
+
+                logs.ToList().ForEach(log =>
+                {
+                    sb.AppendLine($"{log},");
+                });
+
+
+                var content = ObjectToByteArray(sb.ToString());
+
+                return File(content, "text/csv");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        byte[] ObjectToByteArray(object obj)
+        {
+            if (obj == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+namespace blob_demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+
+            ////swagger 
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Blob Demo",
+                        Description = "Blob Demo",
+                        Version = "v1"
+                    });
+            });
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                //../swagger/v1/
+                s.SwaggerEndpoint("../swagger/v1/swagger.json", "Blob Demo");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
